895a8837f248ff30a7c95d92ded9c3ab07f8a1c1
iAzamat <isabirovazamati@gmail.com>
2024-02-12 21:27:29 +0500

    Add seminar 8.2

---
+++ Aop2/src/main/java/com/example/Aop2/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.Aop2.repository;
+
+import com.example.Aop2.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
