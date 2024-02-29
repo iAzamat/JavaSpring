4fc48a30e1a61d9b244f03d7b0968db6b4ea2fbb
iAzamat <isabirovazamati@gmail.com>
2024-03-01 01:04:46 +0500

    Add Seminar 11

---
+++ taskservice/src/main/java/com/example/taskservice/TaskControllerImpl.java
@@ -0,0 +1,40 @@
+package com.example.taskservice;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TaskControllerImpl implements TaskController {
+    private final MeterRegistry meterRegistry;
+
+    public TaskControllerImpl(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @Override
+    public ResponseEntity<String> greeting(String taskname) {
+        return ResponseEntity.ok("Greetings New Task " + taskname);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Task>> getTasks() {
+        meterRegistry.counter("requests_to_tasks_all").increment();
+        // Получение списка задач
+        return ResponseEntity.ok(generateTaskList());
+    }
+
+    private static List<Task> generateTaskList() {
+        final List<Task> list = new ArrayList<>();
+        list.add(new Task(1, "task1"));
+        list.add(new Task(2, "task2"));
+
+        return list;
+     }
+
+}
